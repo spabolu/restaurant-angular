5154e8bb35818e08762850dba79f71bfdf0e77f9
Christopher <Cballenger@steamsport.com>
2020-07-13 20:07:47 -0400

    Fixed the issue with the title not being found.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  title="(Your title here)"
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
