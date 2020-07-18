32278eb9e379b4c8adc2f7439c7fc666caa22b9a
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-17 18:22:14 -0700

    (added header component files, updated appmodule with header, updated appcomponenthtml with header)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
