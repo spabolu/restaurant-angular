0a1f469ad21a366aa6ce94ace30b9f3c76aec059
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-23 20:28:51 -0700

    routing for landingcomponent there now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {ReservationsComponent} from './reservations/reservations.component';
 import {LandingComponent} from './landing/landing.component';
+import {LocationsComponent} from './locations/locations.component';
 
 const routes: Routes = 
 [{path:'reservations', component: ReservationsComponent},
- {path: 'landing', component: LandingComponent}];
+ {path: 'landing', component: LandingComponent},
+ {path: 'locations', component: LocationsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
