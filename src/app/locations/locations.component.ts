c62c15962f292463a8bcd587f1bf5921a9677ad6
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-23 20:35:00 -0700

    locations component

---
+++ src/app/locations/locations.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-locations',
+  templateUrl: './locations.component.html',
+  styleUrls: ['./locations.component.css']
+})
+export class LocationsComponent implements OnInit {
+
+  times: number = 0;
+  pageNumber = 1;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  incrementPage()
+  {
+    this.pageNumber++;
+    console.log(this.pageNumber);
+
+  }
+
+}
