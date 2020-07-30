2a36ccf997eb100de69e6bde97a58d43ab4eb9ee
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-29 23:48:21 -0700

    current checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
+  message = "CURRENTLY UNDER CONSTRUCTION";
+  
   constructor() { }
 
   ngOnInit(): void {
