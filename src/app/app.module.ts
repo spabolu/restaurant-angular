182a4b07713aa3a8e4a2afd34587c607ba1c2be4
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-30 01:16:09 -0700

    second page not under reserv. anymore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import { LocationsComponent } from './locations/locations.component';
-import { SecondPageComponent } from './reservations/second-page/second-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
