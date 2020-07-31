d3d12118f51d2cb9cd41db200109df1456a9f9f0
Sanya <52510432+sanyakhattar@users.noreply.github.com>
2020-07-31 01:11:58 -0700

    added rest of restaurant location markers

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -20,17 +20,35 @@ export class LocationsComponent implements OnInit {
 
 
         L.marker([36.1716, -115.1525]).addTo(map)
-        .bindPopup('Las Vegas')
+        .bindPopup('Las Vegas Restaurant')
         .openPopup();
 
         L.marker([38.8953,-77.1227]).addTo(map)
-        .bindPopup('Washington')
+        .bindPopup('Washington Restaurant')
         .openPopup();
 
         L.marker([32.7754,-96.7999]).addTo(map)
-        .bindPopup('Dallas')
+        .bindPopup('Dallas Restaurant')
         .openPopup();
 
+        L.marker([34.0522,-118.2437]).addTo(map)
+        .bindPopup('Los Angeles Restaurant')
+        .openPopup();
 
+        L.marker([40.7128, -74.0060]).addTo(map)
+        .bindPopup('New York Restaurant')
+        .openPopup();
+
+        L.marker([33.7490, -84.3880]).addTo(map)
+        .bindPopup('Atlanta Restaurant')
+        .openPopup();
+
+        L.marker([33.4484, -112.0740]).addTo(map)
+        .bindPopup('Phoenix Restaurant')
+        .openPopup();
+
+        L.marker([47.6062, -122.3321]).addTo(map)
+        .bindPopup('Seattle Restaurant')
+        .openPopup();
   }
 }
