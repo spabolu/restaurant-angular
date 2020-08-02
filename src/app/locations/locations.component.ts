4c03922fd02c3d267ba425fa6eec5d446f8100dd
Saketh <sakethmadhu7@gmail.com>
2020-08-02 16:35:01 -0700

    update app-locations.ts

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -27,35 +27,5 @@ export class LocationsComponent implements OnInit {
         .bindPopup(this.locations[x].name)
         .openPopup();
     }
-
-
-    /*
-        L.marker([38.8953, -77.1227]).addTo(map)
-          .bindPopup('Washington Restaurant')
-          .openPopup();
-    
-        L.marker([32.7754, -96.7999]).addTo(map)
-          .bindPopup('Dallas Restaurant')
-          .openPopup();
-    
-        L.marker([34.0522, -118.2437]).addTo(map)
-          .bindPopup('Los Angeles Restaurant')
-          .openPopup();
-    
-        L.marker([40.7128, -74.0060]).addTo(map)
-          .bindPopup('New York Restaurant')
-          .openPopup();
-    
-        L.marker([33.7490, -84.3880]).addTo(map)
-          .bindPopup('Atlanta Restaurant')
-          .openPopup();
-    
-        L.marker([33.4484, -112.0740]).addTo(map)
-          .bindPopup('Phoenix Restaurant')
-          .openPopup();
-    
-        L.marker([47.6062, -122.3321]).addTo(map)
-          .bindPopup('Seattle Restaurant')
-          .openPopup(); */
   }
 }
